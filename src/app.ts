8039d6c9df90445b5f57aeff167198818c9046c0
P.Nyiringango <p.nyiringango@equitybank.co.rw>
2023-06-01 12:42:08 +0200

    connection error

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,12 @@ import productRoutes from './routes/ProductRoute';
 import userRoutes from './routes/UserRoute';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables from .env file
-
+dotenv.config(); 
 const app = express();
 const port = 3000;
 console.log("====================",process.env.DATABASE_URL)
-// Create a new Sequelize instance using the database URL and explicit dialect
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres', // Replace with the appropriate database dialect
+  dialect: 'postgres', 
 });
 
 app.use(express.json());
@@ -19,9 +17,8 @@ app.use(express.json());
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
 
-// Sync the database using Sequelize migrations
 sequelize
-  .sync({ alter: true }) // Use alter: true to apply migrations without dropping tables
+  .sync({ alter: true }) 
   .then(() => {
     console.log('Database connected');
     app.listen(port, () => {
